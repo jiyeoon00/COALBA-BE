ef42563849cf3b7ecf9ebf0f0b892613c0749f12
jkde7721 <jkde7721@naver.com>
2022-10-30 21:28:37 +0900

    feat: profile 도메인 생성

---
+++ src/main/java/com/project/coalba/domain/profile/entity/Boss.java
@@ -0,0 +1,34 @@
+package com.project.coalba.domain.profile.entity;
+
+import com.project.coalba.domain.auth.entity.User;
+import com.project.coalba.global.audit.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Boss extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "boss_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String realName;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String phoneNum;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    private String imageUrl;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
