72af8a0f869fe0a19a7f1cde74118a428dc5f890
jkde7721 <jkde7721@naver.com>
2022-10-31 12:07:13 +0900

    feat: substituteReq 도메인 생성

---
+++ src/main/java/com/project/coalba/domain/substituteReq/entity/SubstituteReq.java
@@ -0,0 +1,44 @@
+package com.project.coalba.domain.substituteReq.entity;
+
+import com.project.coalba.domain.profile.entity.Boss;
+import com.project.coalba.domain.profile.entity.Staff;
+import com.project.coalba.domain.schedule.entity.Schedule;
+import com.project.coalba.domain.substituteReq.entity.enums.SubstituteReqStatus;
+import com.project.coalba.global.audit.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SubstituteReq extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "substitute_req_id")
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String reqMessage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SubstituteReqStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private Staff receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Staff sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boss_id")
+    private Boss boss;
+}
---
+++ src/main/java/com/project/coalba/domain/substituteReq/entity/enums/SubstituteReqStatus.java
@@ -0,0 +1,6 @@
+package com.project.coalba.domain.substituteReq.entity.enums;
+
+public enum SubstituteReqStatus {
+
+    WAITING, ACCEPTANCE, REFUSAL, APPROVAL, DISAPPROVAL
+}
