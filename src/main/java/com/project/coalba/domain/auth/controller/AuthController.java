252971c8b29b1923612d41a9f549ed57581a9b79
jkde7721 <jkde7721@naver.com>
2022-11-05 11:15:07 +0900

    feat: 로그인 시 구글, 네이버 사용자 정보 받아와 업데이트 or 저장
    기존 사용자는 업데이트, 새로운 사용자는 저장

--- src/main/java/com/project/coalba/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/coalba/domain/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.project.coalba.domain.auth.controller;
 
+import com.project.coalba.domain.auth.entity.enums.Provider;
+import com.project.coalba.domain.auth.entity.enums.Role;
 import com.project.coalba.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,8 +18,8 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login() {
-        authService.login();
+    public ResponseEntity<Void> login(@RequestParam Provider provider, @RequestParam String token, @RequestParam Role role) {
+        authService.login(provider, token, role);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/com/project/coalba/domain/auth/service/AuthService.java
+++ src/main/java/com/project/coalba/domain/auth/service/AuthService.java
@@ -1,5 +1,10 @@
 package com.project.coalba.domain.auth.service;
 
+import com.project.coalba.domain.auth.entity.User;
+import com.project.coalba.domain.auth.entity.enums.Provider;
+import com.project.coalba.domain.auth.entity.enums.Role;
+import com.project.coalba.domain.auth.info.UserInfo;
+import com.project.coalba.domain.auth.info.UserInfoFactory;
 import com.project.coalba.domain.auth.repository.UserRefreshTokenRepository;
 import com.project.coalba.domain.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +19,20 @@ public class AuthService {
     private final UserRefreshTokenRepository userRefreshTokenRepository;
 
     @Transactional
-    public Object login() {
-        //TODO: 구글 or 네이버로부터 받아온 사용자 정보 저장 or 업데이트 후 token 발행
+    public Object login(Provider provider, String token, Role role) {
+        User socialUser = getSocialUser(provider, token, role), loginUser;
+        String providerId = socialUser.getProviderId();
+        boolean isNewUser;
+        if (isSubscribedUser(providerId)) {
+            loginUser = updateUser(socialUser);
+            isNewUser = false;
+        }
+        else {
+             loginUser = joinUser(socialUser);
+             isNewUser = true;
+        }
+
+        //TODO: token 발행 후 DTO 생성해서 반환
         return new Object();
     }
 
@@ -23,5 +40,24 @@ public class AuthService {
     public Object reissue() {
         //TODO: refresh token 유효성 검사 후 token 재발행
         return new Object();
+    }
+
+    private User getSocialUser(Provider provider, String token, Role role) {
+        UserInfo userInfo = UserInfoFactory.getUserInfo(provider);
+        return userInfo.getUser(token, role);
+    }
+
+    private boolean isSubscribedUser(String providerId) {
+        return userRepository.findByProviderId(providerId).isPresent(); //영속성 컨텍스트에서 관리X
+    }
+
+    private User updateUser(User socialUser) {
+        User loginUser = userRepository.findByProviderId(socialUser.getProviderId()).get();
+        loginUser.updateSocialInfo(socialUser);
+        return loginUser;
+    }
+
+    private User joinUser(User socialUser) {
+        return userRepository.save(socialUser);
     }
 }
