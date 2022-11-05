bb6d382c8664e6b8dd762021e9b74289b79e8536
jkde7721 <jkde7721@naver.com>
2022-11-05 12:28:34 +0900

    add: JWT 토큰 Manager 생성
    - 토큰 생성
    - 토큰 기반 인증 객체(Authentication) 생성
    - 토큰 유효성 검사
    - 일반 토큰 Claims, 만료된 토큰 Claims parsing

---
+++ src/main/java/com/project/coalba/domain/auth/entity/UserPrincipal.java
@@ -0,0 +1,61 @@
+package com.project.coalba.domain.auth.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter @Builder
+public class UserPrincipal implements UserDetails {
+
+    private final Long userId;
+    private final String providerId;
+    private final Collection<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return providerId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static UserPrincipal create(User user) {
+        return UserPrincipal.builder()
+                .userId(user.getId())
+                .providerId(user.getProviderId())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getCode())))
+                .build();
+    }
+}
