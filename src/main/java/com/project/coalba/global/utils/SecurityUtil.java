b3e8d7ba91be835889d4c9ddc3a44507531fb4ee
jkde7721 <jkde7721@naver.com>
2022-11-13 14:06:39 +0900

    add: security context에서 인증된 사용자 정보 조회하는 utils 추가

---
+++ src/main/java/com/project/coalba/global/utils/SecurityUtil.java
@@ -0,0 +1,18 @@
+package com.project.coalba.global.utils;
+
+import com.project.coalba.domain.auth.entity.UserPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+
+    private SecurityUtil() {}
+
+    public static Long getCurrentUserId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+        }
+        return ((UserPrincipal) authentication.getPrincipal()).getUserId();
+    }
+}
