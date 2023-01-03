23768cb56c0fcb8400548621bd07913e6cef6168
jkde7721 <jkde7721@naver.com>
2023-01-03 14:59:45 +0900

    add: ScheduleStatus 설명 주석 추가

--- src/main/java/com/project/coalba/domain/schedule/entity/enums/ScheduleStatus.java
+++ src/main/java/com/project/coalba/domain/schedule/entity/enums/ScheduleStatus.java
@@ -1,8 +1,12 @@
 package com.project.coalba.domain.schedule.entity.enums;
 
 public enum ScheduleStatus {
-    
-    //SUCCESS: 정상 출퇴근한 경우
-    //FAIL: 출석부 수정 요청 가능한 경우 ex. 지각, 조기퇴근, 연장근무, 무단결근
+    /**
+     * <b>BEFORE_WORK</b> 스케줄 시작 전 <br>
+     * <b>ON_DUTY</b> 스케줄 진행 중 (정시 출근 후) <br>
+     * <b>LATE</b> 스케줄 진행 중 (지각 출근 후) <br>
+     * <b>SUCCESS</b> 스케줄 완료 <br>
+     * <b>FAIL</b> 스케줄 미완료 (ex. 지각, 조기퇴근, 결근 등) <br>
+     */
     BEFORE_WORK, ON_DUTY, LATE, SUCCESS, FAIL
 }
