280265c34534cd17aa40e2fcdfcde79506857c87
jkde7721 <jkde7721@naver.com>
2023-01-05 11:58:48 +0900

    refactor: ScheduleDateTime에 대한 반복 제거
    ScheduleDateTime에 검증 어노테이션 적용 후, ScheduleCreateRequest에서
    기존 scheduleStartDateTime, scheduleEndDateTime 필드를 ScheduleDateTime으로 대체

--- src/main/java/com/project/coalba/domain/schedule/dto/request/ScheduleDateTime.java
+++ src/main/java/com/project/coalba/domain/schedule/dto/request/ScheduleDateTime.java
@@ -1,19 +1,22 @@
 package com.project.coalba.domain.schedule.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.coalba.domain.schedule.validation.*;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
+@ValidScheduleDateTime
 public class ScheduleDateTime {
 
-    @NotNull
+    @ValidScheduleDate
+    @NotNull @ValidTimeMinUnit
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime start;
 
-    @NotNull
+    @NotNull @ValidTimeMinUnit
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime end;
 }
