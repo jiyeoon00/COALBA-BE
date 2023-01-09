6d8616fc08a10cb2669b506c94344a9c7aabb689
jkde7721 <jkde7721@naver.com>
2023-01-10 00:27:56 +0900

    fix: Boss Message API path 오타 수정

--- src/main/java/com/project/coalba/domain/message/controller/BossMessageController.java
+++ src/main/java/com/project/coalba/domain/message/controller/BossMessageController.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.*;
 public class BossMessageController {
     private final BossMessageService bossMessageService;
 
-    @PostMapping("/workspaces/{workspaceId}/staffs/{staffId})")
+    @PostMapping("/workspaces/{workspaceId}/staffs/{staffId}")
     public ResponseEntity<Void> sendMessageToStaff(@PathVariable Long workspaceId, @PathVariable Long staffId,
                                                    @RequestBody String content) {
         bossMessageService.sendMessageToStaff(workspaceId, staffId, content);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/workspaces/{workspaceId}/staffs/{staffId})")
+    @GetMapping("/workspaces/{workspaceId}/staffs/{staffId}")
     public MessageResponse.BossMessageResponse getDetailMessages(@PathVariable Long workspaceId, @PathVariable Long staffId) {
         return bossMessageService.getDetailMessages(workspaceId, staffId);
     }
